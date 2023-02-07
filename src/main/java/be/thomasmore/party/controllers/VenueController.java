e88679df7dff90e1dab93057898e9ab58ba685a2
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-02-07 10:58:25 +0100

    alle filters verwijderd - 1.2

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -43,46 +43,4 @@ public class VenueController {
         model.addAttribute("venues", allVenues);
         return "venuelist";
     }
-
-    @GetMapping({"/venuelist/outdoor/{filter}", "/venuelist/outdoor"})
-    public String venueListOutdoor(Model model, @PathVariable(required = false) String filter) {
-        boolean boolFilter = true;
-        if (filter!=null && (filter.equals("no") || filter.equals("false"))) boolFilter = false;
-        Iterable<Venue> venues = venueRepository.findByOutdoor(boolFilter);
-        model.addAttribute("outdoorFilter", boolFilter);
-        model.addAttribute("venues", venues);
-        return "venuelist";
-    }
-
-    @GetMapping({"/venuelist/indoor/{filter}", "/venuelist/indoor"})
-    public String venueListIndoor(Model model, @PathVariable(required = false) String filter) {
-        boolean boolFilter = true;
-        if (filter!=null && (filter.equals("no") || filter.equals("false"))) boolFilter = false;
-        Iterable<Venue> venues = venueRepository.findByIndoor(boolFilter);
-        model.addAttribute("indoorFilter", boolFilter);
-        model.addAttribute("venues", venues);
-        return "venuelist";
-    }
-
-    @GetMapping({"/venuelist/size/{filter}", "/venuelist/size"})
-    public String venueListSize(Model model, @PathVariable(required = false) String filter) {
-        if (filter==null) filter = "all";
-        if (filter.equals("s")) filter = "S";
-        if (filter.equals("m")) filter = "M";
-        if (filter.equals("l")) filter = "L";
-        if (!filter.equals("S") && !filter.equals("M") && !filter.equals("L")) filter = "all";
-        Iterable<Venue> venues;
-        if (filter.equals("all")) {
-            venues = venueRepository.findAll();
-        } else if (filter.equals("S")) {
-            venues = venueRepository.findByCapacityLessThanEqual(200);
-        } else if (filter.equals("M")) {
-            venues = venueRepository.findByCapacityIsBetween(200, 500);
-        } else {
-            venues = venueRepository.findByCapacityIsGreaterThan(500);
-        }
-        model.addAttribute("sizeFilter", filter);
-        model.addAttribute("venues", venues);
-        return "venuelist";
-    }
 }
