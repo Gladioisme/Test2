783a3cbef27c8f4585cef2e3105e65c2dd4548d6
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-03-14 11:39:02 +0100

    animal alle oefeningen

---
+++ src/main/java/be/thomasmore/party/repositories/AnimalRepository.java
@@ -0,0 +1,15 @@
+package be.thomasmore.party.repositories;
+
+import be.thomasmore.party.model.Animal;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AnimalRepository extends CrudRepository<Animal, Integer> {
+    Optional<Animal> findFirstByIdLessThanOrderByIdDesc(int id);
+    Optional<Animal> findFirstByIdGreaterThanOrderById(int id);
+    Optional<Animal> findFirstByOrderByIdDesc();
+    Optional<Animal> findFirstByOrderByIdAsc();
+    List<Animal> findAllBy();
+}
